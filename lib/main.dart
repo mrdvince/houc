d234aef09d4d656ea6de7ada47bcda1c3cd93738
Droid021 <vincivenv@gmail.com>
2021-02-01 12:56:45 +0300

    Page controller

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,36 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List slides = getSlides();
+  PageController pageController = new PageController();
+  int slideIndex = 0;
+
+  // @override
+  // void initState() {
+  //   List slides = getSlides();
+  //
+  // }
+
   @override
   Widget build(BuildContext context) {
     final mq = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: Color(0xFF301E2F),
-      body: _ListTileWidget(mq: mq, slides: slides, index: 3,),
+      body: Container(
+        height: mq.size.height - 100,
+        child: PageView(
+            onPageChanged: (index) {
+              setState(() {
+                slideIndex = index;
+              });
+            },
+            controller: pageController,
+            children: [
+              _ListTileWidget(mq: mq, slides: slides, index: 0),
+              _ListTileWidget(mq: mq, slides: slides, index: 1),
+              _ListTileWidget(mq: mq, slides: slides, index: 2),
+              _ListTileWidget(mq: mq, slides: slides, index: 3),
+            ]),
+      ),
     );
   }
 }
@@ -45,7 +69,7 @@ class _ListTileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: mq.size.height -100,
+        height: mq.size.height - 100,
         padding: EdgeInsets.symmetric(horizontal: 20),
         alignment: Alignment.center,
         child: Column(
@@ -55,20 +79,24 @@ class _ListTileWidget extends StatelessWidget {
             SizedBox(
               height: 40,
             ),
-            Text(slides[index].title, textAlign: TextAlign.center,style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color: Color(0xFFFFAD3A),
-                fontSize: 26
-            ),),
+            Text(
+              slides[index].title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFFFFAD3A),
+                  fontSize: 26),
+            ),
             SizedBox(
               height: 20,
             ),
-            Text(slides[index].desc, textAlign: TextAlign.center,style: TextStyle(
-                fontWeight: FontWeight.w500,
-                color: Color(0xFF524D63),
-                fontSize: 18))
+            Text(slides[index].desc,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF524D63),
+                    fontSize: 18))
           ],
         ));
   }
 }
-
