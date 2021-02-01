0e83820232f3ac104172fd0eae5b01b051966dc2
Droid021 <vincivenv@gmail.com>
2021-02-01 10:18:36 +0300

    Project setup

---
+++ android/app/src/main/kotlin/com/example/maalhous/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maalhous
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
