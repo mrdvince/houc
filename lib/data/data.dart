dae2c04f65664a03e6d5ed04fadc5f1c6af43175
Droid021 <vincivenv@gmail.com>
2021-02-01 12:40:17 +0300

    List tile widget

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -30,38 +30,43 @@ class SliderModel {
     return desc;
   }
 }
+
 List<SliderModel> getSlides() {
   List<SliderModel> slides = new List<SliderModel>();
   SliderModel sliderModel = new SliderModel();
-//1
-  sliderModel.setDesc("Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus.");
+//0
+  sliderModel.setDesc(
+      "Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus.");
   sliderModel.setTitle("Post your application");
   sliderModel.setImageAssetPath("assets/Image-05.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
 
-  //2
-  sliderModel.setDesc("Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus.");
+  //1
+  sliderModel.setDesc(
+      "Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus.");
   sliderModel.setTitle("Broadcast your application");
   sliderModel.setImageAssetPath("assets/Image-06.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
-
-  //3
-  sliderModel.setDesc("Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus.");
+  //2
+  sliderModel.setDesc(
+      "Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus. ");
   sliderModel.setTitle("Choose the best deal");
   sliderModel.setImageAssetPath("assets/Image-07.png");
   slides.add(sliderModel);
 
-  //4
-  sliderModel.setDesc("Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus. ");
+  sliderModel = new SliderModel();
+
+  //2
+  sliderModel.setDesc(
+      "Praesent ultrices vitae nisi in tempor. Sed et nibh congue, tempus odio accumsan, fringilla tellus. ");
   sliderModel.setTitle("Get your application approved");
   sliderModel.setImageAssetPath("assets/Image-08.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
-
   return slides;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -22,34 +22,53 @@ class _HomeState extends State<Home> {
   List slides = getSlides();
   @override
   Widget build(BuildContext context) {
-    print(slides.length);
     final mq = MediaQuery.of(context);
     return Scaffold(
       backgroundColor: Color(0xFF301E2F),
-      body: Container(
-        height: mq.size.height -100,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(slides[0].imageAssetPath),
-              SizedBox(
-                height: 40,
-              ),
-              Text(slides[0].title, textAlign: TextAlign.center,style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFFFFAD3A),
-                  fontSize: 26
-              ),),
-              SizedBox(
-                height: 20,
-              ),
-              Text(slides[0].desc, textAlign: TextAlign.center,style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  color: Color(0xFF524D63),
-                  fontSize: 18))
-            ],
-          )),
+      body: _ListTileWidget(mq: mq, slides: slides, index: 3,),
     );
+  }
+}
+
+class _ListTileWidget extends StatelessWidget {
+  const _ListTileWidget({
+    Key key,
+    @required this.mq,
+    @required this.slides,
+    @required this.index,
+  }) : super(key: key);
+
+  final MediaQueryData mq;
+  final List slides;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: mq.size.height -100,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(slides[index].imageAssetPath),
+            SizedBox(
+              height: 40,
+            ),
+            Text(slides[index].title, textAlign: TextAlign.center,style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Color(0xFFFFAD3A),
+                fontSize: 26
+            ),),
+            SizedBox(
+              height: 20,
+            ),
+            Text(slides[index].desc, textAlign: TextAlign.center,style: TextStyle(
+                fontWeight: FontWeight.w500,
+                color: Color(0xFF524D63),
+                fontSize: 18))
+          ],
+        ));
   }
 }
 
