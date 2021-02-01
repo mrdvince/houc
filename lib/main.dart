3a941060e32650aecabbc93440812ac6ef5979fa
Droid021 <vincivenv@gmail.com>
2021-02-01 11:01:10 +0300

    widget indicator, initstate
    initstate -> slides and pagecontroller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maalhous/data/data.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,12 +12,35 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List<SliderModel> mySlides = <SliderModel>[];
+  int slideIdx = 0;
+  PageController _pageController;
+
+  Widget _builWidgetIndicator(bool isCurrentPage) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0),
+      height: isCurrentPage ? 10.0 : 6.0,
+      width: isCurrentPage ? 10.0 : 6.0,
+      decoration: BoxDecoration(
+          color: isCurrentPage ? Colors.grey : Colors.grey[300],
+          borderRadius: BorderRadius.circular(12.0)),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    mySlides = getSlides();
+    _pageController = new PageController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
