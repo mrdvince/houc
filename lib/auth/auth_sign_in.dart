3448fb210e89c82528f119fc31e171e95e7fa258
Droid021 <vincivenv@gmail.com>
2021-02-02 16:32:56 +0300

    auth: Sign up

---
+++ lib/auth/auth_sign_in.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:maalhous/home/user_home.dart';
+import 'package:provider/provider.dart';
+import 'auth_service.dart';
+
+import 'auth_sign_up.dart';
+import 'widgets/auth_alert_dialog.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+
+    void showAlertDialog(BuildContext context) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            TextEditingController _emailControllerField =
+                TextEditingController();
+            return CustomAlertDialog(
+              content: Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                height: MediaQuery.of(context).size.height / 4.5,
+                color: Colors.white,
+                child: Column(
+                  children: <Widget>[
+                    Text("Insert Reset Email:"),
+                    TextField(
+                      controller: _emailControllerField,
+                      decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+                        ),
+                        hintText: "something@example.com",
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(15),
+                      child: Material(
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Color(0xff8c52ff),
+                        child: MaterialButton(
+                          minWidth: mq.size.width / 2,
+                          padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+                          child: Text(
+                            "Send Reset Email",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () async {
+                            try {
+                              FirebaseAuth.instance.sendPasswordResetEmail(
+                                  email: _emailControllerField.text);
+                              Navigator.of(context).pop();
+                            } catch (e) {
+                              print(e);
+                              // TODO: Add snackbar reporting error
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          });
+    }
+
+    final logo = Image.asset(
+      "assets/logo.png",
+      height: mq.size.height / 4,
+    );
+
+    final emailField = TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintText: "something@example.com",
+        labelText: "Email",
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final passwordField = Column(
+      children: <Widget>[
+        TextFormField(
+          obscureText: true,
+          controller: _passwordController,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          cursorColor: Colors.white,
+          decoration: InputDecoration(
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: Colors.white,
+              ),
+            ),
+            hintText: "password",
+            labelText: "Password",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+            hintStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(2.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            MaterialButton(
+                child: Text(
+                  "Forgot Password",
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption
+                      .copyWith(color: Colors.white),
+                ),
+                onPressed: () {
+                  showAlertDialog(context);
+                }),
+          ],
+        ),
+      ],
+    );
+
+    final fields = Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          emailField,
+          passwordField,
+        ],
+      ),
+    );
+
+    final loginButton = ButtonTheme(
+      minWidth: mq.size.width / 1.2,
+      height: 50.0,
+      child: FlatButton(
+        onPressed: () async {
+          // TODO: firebase login
+          context.read<AuthenticationService>().signIn(
+                email: _emailController.text.trim(),
+                password: _passwordController.text.trim(),
+              );
+          // Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+          Navigator.of(context).pushAndRemoveUntil(
+              MaterialPageRoute(builder: (context) => UserHome()),
+              (route) => false);
+        },
+        child: const Text(
+          'Login',
+          style: TextStyle(color: Colors.white),
+        ),
+        color: Color(0xFFFFAD3A),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final bottom = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        loginButton,
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Not a member?",
+              style: Theme.of(context).textTheme.subtitle1.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Register()));
+              },
+              child: Text(
+                "Sign Up",
+                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Color(0xFF301E2F),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(36),
+          child: Container(
+            height: mq.size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                logo,
+                fields,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 150),
+                  child: bottom,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
