baf255d814cc13cd8a6aad584a8dd30295715300
Droid021 <vincivenv@gmail.com>
2021-02-02 16:37:43 +0300

    feat: Authentication wrapper
    Wrap widget tree with Provider, check if user is loged
    navigate accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:maalhous/home/user_home.dart';
+import 'package:maalhous/onboarding/slides.dart';
+import 'package:provider/provider.dart';
+import 'package:maalhous/auth/auth_service.dart';
 
-import 'onboarding/slides.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(App());
+}
 
-void main() => runApp(MaalHous());
-
-class MaalHous extends StatelessWidget {
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Onboard(),
-      debugShowCheckedModeBanner: false,
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
     );
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        home: AuthenticationWrapper(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  // const AuthenticationWrapper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+    if (firebaseUser != null) {
+      return UserHome();
+    }
+    return Onboard();
   }
 }
