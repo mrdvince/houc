9e91bfff1a4e7b2b0f3cc492abd0f835265cee35
Droid021 <vincivenv@gmail.com>
2021-02-02 16:33:12 +0300

    auth: Sign up

---
+++ lib/auth/auth_sign_up.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+import 'package:maalhous/home/user_home.dart';
+import 'package:provider/provider.dart';
+import 'auth_service.dart';
+import 'auth_sign_in.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterViewState createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _repasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+
+    final logo = Image.asset(
+      "assets/logo.png",
+      height: mq.size.height / 4,
+    );
+
+    final usernameField = TextFormField(
+      controller: _usernameController,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintText: "John Doe",
+        labelText: "Username",
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final emailField = TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintText: "something@example.com",
+        labelText: "Email",
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final passwordField = TextFormField(
+      obscureText: true,
+      controller: _passwordController,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintText: "password",
+        labelText: "Password",
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final repasswordField = TextFormField(
+      obscureText: true,
+      controller: _repasswordController,
+      style: TextStyle(
+        color: Colors.white,
+      ),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+          ),
+        ),
+        hintText: "password",
+        labelText: "Re-enter Password",
+        labelStyle: TextStyle(
+          color: Colors.white,
+        ),
+        hintStyle: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+
+    final fields = Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          usernameField,
+          emailField,
+          passwordField,
+          repasswordField,
+        ],
+      ),
+    );
+
+    final registerButton = ButtonTheme(
+      minWidth: mq.size.width / 1.2,
+      height: 50.0,
+      child: FlatButton(
+        onPressed: () async {
+          // TODO: firebase register
+          try {
+            context.read<AuthenticationService>().signUp(
+                  email: _emailController.text.trim(),
+                  password: _passwordController.text.trim(),
+                );
+            // Navigator.of(context).pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context) => UserHome()),
+                (route) => false);
+          } catch (e) {
+            print(e);
+            _usernameController.text = "";
+            _passwordController.text = "";
+            _repasswordController.text = "";
+            _emailController.text = "";
+            // TODO: alertdialog with error
+            return e.message;
+          }
+        },
+        child: const Text(
+          'Register',
+          style: TextStyle(color: Colors.white),
+        ),
+        color: Color(0xFFFFAD3A),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+
+    final bottom = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        registerButton,
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Already have an account?",
+              style: Theme.of(context).textTheme.subtitle1.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Login()));
+              },
+              child: Text(
+                "Login",
+                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Color(0xFF301E2F),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(36),
+          child: Container(
+            height: mq.size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                logo,
+                fields,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 150),
+                  child: bottom,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
