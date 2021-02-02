a66919e0a6697dee4625844b0a6b59b6ce0b0f32
Droid021 <vincivenv@gmail.com>
2021-02-02 16:32:05 +0300

    auth service

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -6,14 +6,16 @@ class AuthenticationService {
   AuthenticationService(this._firebaseAuth);
 
   Stream<User> get authStateChanges => _firebaseAuth.idTokenChanges();
-  
+
   Future<String> signIn({String email, String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
       print("Signed in");
+      return "Signed in";
     } on FirebaseAuthException catch (e) {
       print(e.message);
+      return "Signed in";
     }
   }
 
@@ -22,13 +24,16 @@ class AuthenticationService {
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       print("Signed up");
+      return "Signed in";
     } on FirebaseAuthException catch (e) {
       print(e.message);
+      return "Signed in";
     }
   }
 
   Future<String> signOut() async {
     await _firebaseAuth.signOut();
     print("Log out");
+    return "Signed in";
   }
 }
